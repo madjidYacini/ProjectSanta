7557111d2bb6d24d2f93bda10df03702472b3a12
madjid yacini <madjid.yacini@gmail.com>
2018-02-23 17:54:44 +0100

    second version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ProjectSanta
+#step 1 : DONE
+#STEP 2 : ALMOST
+# STEP3 : HELL NO !
--- class.Elf.swift
+++ class.Elf.swift
@@ -16,18 +16,13 @@ class Elf : Packaging, Toy {
  func pack(package : Packaging, toy : Toy) {
    if ((package.isOpen() == false) {
      print("Sorry this package is not open")
-   } else if (emballage possède déjà un cadeau){
-     print("Sorry this package already filled")
-   } else {
+   } else{
      print("Yeaaaah! Just packing the toy ~~ X ~~")
    }
  }
 
 
  func unPack(packageL : Packaging) {
-   if(emballage est ouvert) {
-     print("Sorry this package is already empty")
-   } else {
      print("Ooooooh! Just unpacking the toy ~~ X ~~")
    }
  }
